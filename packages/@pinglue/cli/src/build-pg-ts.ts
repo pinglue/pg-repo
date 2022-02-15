88b3db302f53bb048ff62e256f81339ffe92eed1
Amin <amin.di.sam@gmail.com>
2022-02-15 10:23:41 -0500

    generate pg-gen.ts based on yaml file

--- packages/@pinglue/cli/src/build-pg-ts.ts
+++ packages/@pinglue/cli/src/build-pg-ts.ts
@@ -1,1 +1,24 @@
-// TODO
+import Handlebars from "handlebars";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
+// import prettier from "prettier";
+
+const camelToKebab = (str) =>
+    str.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
+
+const file = fs.readFileSync(path.resolve("src", "template.hbs"), "utf-8");
+const template = Handlebars.compile(file);
+
+const yamlFile = fs.readFileSync(path.resolve("src", "cmds.yaml"), "utf8");
+let parsedYaml = YAML.parse(yamlFile);
+parsedYaml.commands = parsedYaml.commands.map((i) => ({
+    ...i,
+    fileName: camelToKebab(i.action) + ".js"
+}));
+
+const vars = parsedYaml;
+
+const result = template(vars);
+// const formattedCode = prettier.format(result, { parser: "babel-ts" });
+fs.writeFileSync(path.resolve("src", "pg-gen.ts"), result);
