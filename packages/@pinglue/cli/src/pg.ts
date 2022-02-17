a61f1f0856021535ea0d9bdd2cfa2895c5bd048e
Amin <23167933+aminsaedi@users.noreply.github.com>
2022-02-16 22:25:00 -0500

    feat(cli): merge PR #30
    * generate pg-gen.ts based on yaml file
    
    * update template and commands
    
    * rename action
    
    * read version from package.json
    
    * remoe unnecessary file
    
    * use prettier to format generated code
    
    * fix issues
    
    * final adjustments
    
    Co-authored-by: Payam <payamv@google.com>

--- packages/@pinglue/cli/src/pg.ts
+++ packages/@pinglue/cli/src/pg.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
+/**
+ * THIS FILE IS GENERATED! DO NOT MODIFY MANUALLY
+ * To change, use the script `scripts/build-pg.js`
+ * or `npm run build-pg` at the package root
+ */
+
 import "source-map-support/register.js";
 
-import {Command} from "commander";
-import {
-    print,
-    style,
-    commanderHelp as help
-} from "@pinglue/print/node";
+import { Command } from "commander";
+import { print, style, commanderHelp as help } from "@pinglue/print/node";
 
 // action functions
 import infoAction from "./actions/info.js";
-import runRouteAction from "./actions/run-route.js";
+import runAction from "./actions/run.js";
+import exportAction from "./actions/export.js";
 import envTempAction from "./actions/env-temp.js";
 import channelsAction from "./actions/channels.js";
 import channelsManAction from "./actions/channels-man.js";
-import exportAction from "./actions/export.js";
-import dumpRegAction from "./actions/dump-registry.js";
+import dumpRegistryAction from "./actions/dump-registry.js";
 
 const program = new Command();
 
@@ -29,69 +31,131 @@ program.addHelpText("beforeAll", style.mute("\n~~~~~ Pinglue CLI ~~~~~\n"));
 program.addHelpText("afterAll", style.mute("\nHave a nice life! \n"));
 
 program
-    .command("info [route-name]")
-    .description("Show info about packages. If route provided then the info will be about that route, otherwise it'll be about general info about all routes")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .option("-k, --pkg <package-name>", "Show info only for this package")
-    .action(infoAction({style, print}));
+  .command("info [route-name]")
+  .description(
+    "Show info about packages. If route provided then the info will be about that route, otherwise it'll be about general info about all routes"
+  )
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .option("-k, --pkg <package-name>", "Show info only for this package")
+  .action(infoAction({ style, print }));
 
 program
-    .command("run [route-name]")
-    .description("Run the given route (defaults to /)")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .option("-s, --silent", "Print nothing to the screen (for the registry and factory)")
-    .option("-f, --file-log", "Use file logger as the local logger")
-    .option("--no-print-logs", "Not using print logger as a local logger")
-    .action(runRouteAction({print, style}));
+  .command("run [route-name]")
+  .description("Run the given route (defaults to /)")
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .option(
+    "-s, --silent",
+    "Print nothing to the screen (for the registry and factory)"
+  )
+  .option("-f, --file-log", "Use file logger as the local logger")
+  .option("--no-print-logs", "Not using print logger as a local logger")
+  .action(runAction({ style, print }));
 
 program
-    .command("export [route-name]")
-    .description("generate code for the given route (defaults to /)")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .option("-s, --silent", "Print nothing to the screen (for the registry and factory)")
-    .option("-f, --file-log", "Use file logger as the local logger")
-    .option("--no-print-logs", "Not using print logger as a local logger")
-    .option("-b, --browser", "Browser version of the code")
-    .option("-o, --output <filename>", "Output the code to the given file")
-    .action(exportAction({print, style}));
+  .command("export [route-name]")
+  .description("generate code for the given route (defaults to /)")
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .option(
+    "-s, --silent",
+    "Print nothing to the screen (for the registry and factory)"
+  )
+  .option("-f, --file-log", "Use file logger as the local logger")
+  .option("--no-print-logs", "Not using print logger as a local logger")
+  .option("-b, --browser", "Browser version of the code")
+  .option("-o, --output <filename>", "Output the code to the given file")
+  .action(exportAction({ style, print }));
 
 program
-    .command("env-temp")
-    .description("Create a template env file based on plugins env info (found in pg.yaml)")
-    .option("-n, --name <filename>", "Name of the environment. Default: temp")
-    .option("-s, --show", "Just show the env template file on screen without writing it into the disk.")
-    .action(envTempAction({print, style}));
+  .command("env-temp")
+  .description(
+    "Create a template env file based on plugins env info (found in pg.yaml)"
+  )
+  .option("-n, --name <filename>", "Name of the environment. Default: temp")
+  .option(
+    "-s, --show",
+    "Just show the env template file on screen without writing it into the disk."
+  )
+  .action(envTempAction({ style, print }));
 
 program
-    .command("channels [route-name]")
-    .description("Show a realtime channel report for the given route (defaults to /) by creating a hub and show its channel report")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .action(channelsAction({print, style}));
+  .command("channels [route-name]")
+  .description(
+    "Show a realtime channel report for the given route (defaults to /) by creating a hub and show its channel report"
+  )
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .action(channelsAction({ style, print }));
 
 program
-    .command("channels-man [route-name]")
-    .description("Generetaes channel manual for the given route (defaults to /) based on yaml files in the channels folder of the installed pg modules")
-    .option("-k, --pkg <package-name>", "Filter channels by the registering package")
-    .option("-c, --channel <channel-name>", "Filter by the channel name")
-    .option("-f, --full", "Show the full report for each channel (including description, params, return, etc.)")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .action(channelsManAction({print, style}));
+  .command("channels-man [route-name]")
+  .description(
+    "Generates channel manual for the given route (defaults to /) based on yaml files in the channels folder of the installed pg modules"
+  )
+  .option(
+    "-k, --pkg <package-name>",
+    "Filter channels by the registering package"
+  )
+  .option("-c, --channel <channel-name>", "Filter by the channel name")
+  .option(
+    "-f, --full",
+    "Show the full report for each channel (including description, params, return, etc.)"
+  )
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .action(channelsManAction({ style, print }));
 
 program
-    .command("dump-registry [route-name]")
-    .description("Dumps packages of the registry")
-    .option("-e, --env <env-type>", "Specifying the environment for the hub (defaults to local)")
-    .option("-p, --profiles <profiles>", "Specifying the profiles for the hub (defaults to dev)")
-    .option("-k, --pkg <package-name>", "Show the registry record only for this package")
-    .option("--no-import", "Registry option")
-    .option("--no-dataPath", "Registry option")
-    .option("--no-channels", "Registry option")
-    .option("--no-settings", "Registry option")
-    .action(dumpRegAction({print, style}));
+  .command("dump-registry [route-name]")
+  .description("Dumps packages of the registry")
+  .option(
+    "-e, --env <env-type>",
+    "Specifying the environment for the hub (defaults to local)"
+  )
+  .option(
+    "-p, --profiles <profiles>",
+    "Specifying the profiles for the hub (defaults to dev)"
+  )
+  .option(
+    "-k, --pkg <package-name>",
+    "Show the registry record only for this package"
+  )
+  .option("--no-import", "Registry option")
+  .option("--no-dataPath", "Registry option")
+  .option("--no-channels", "Registry option")
+  .option("--no-settings", "Registry option")
+  .action(dumpRegistryAction({ style, print }));
 
 program.parse();
