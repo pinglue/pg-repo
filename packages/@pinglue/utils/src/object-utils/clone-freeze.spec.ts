68b6f91fb27a5f7c089fe1de4a66be8863652d21
Amin <amin.di.sam@gmail.com>
2022-03-31 22:53:49 -0400

    Fix issues

--- packages/@pinglue/utils/src/object-utils/clone-freeze.spec.ts
+++ packages/@pinglue/utils/src/object-utils/clone-freeze.spec.ts
@@ -1,77 +1,81 @@
 import { expect } from "chai";
 import { _cloneFreeze } from "./index.js";
 
-it("Test cloneFreeze function", () => {
+describe("cloneFreeze", () => {
 
-    const obj = {
-        admin: false,
-        config1: { a: { t: 1 } },
-        config2: null,
-        config3: undefined,
-        arrField: [1, 2, "hey1", true],
-        author: {
-            name: { last: "Smith" },
-            username: "joesmith"
+    it("Test cloneFreeze function", () => {
+
+        const obj = {
+            admin: false,
+            config1: { a: { t: 1 } },
+            config2: null,
+            config3: undefined,
+            arrField: [1, 2, "hey1", true],
+            author: {
+                name: { last: "Smith" },
+                username: "joesmith"
+            }
+        };
+
+        const ans = _cloneFreeze(obj);
+
+        // clone
+        expect(ans).to.deep.equal(obj);
+
+        // deep clone
+        expect(ans).not.to.equal(obj);
+        expect(ans.author).not.to.equal(obj.author);
+        expect(ans.arrField).not.to.equal(obj.arrField);
+        expect(ans.config1.a).not.to.equal(obj.config1.a);
+
+        // frozen
+        expect(() => {
+
+            ans.admin = false;
+
+        }).throw();
+
+        expect(() => {
+
+            ans.admin = true;
+
+        }).throw();
+
+        // no effect
+        obj.author.username = "new-name";
+        expect(ans.author.username).to.equal("joesmith");
+        obj.arrField.push("ggg");
+        expect(ans.arrField.length).to.equal(4);
+
+        /*try {
+            ans.config1.a.t = 2;
         }
-    };
+        catch(error) {
+            console.log(error);
+        }*/
 
-    const ans = _cloneFreeze(obj);
+        // deep frozen
+        expect(() => {
 
-    // clone
-    expect(ans).to.deep.equal(obj);
+            ans.config1.a.t = 2;
 
-    // deep clone
-    expect(ans).not.to.equal(obj);
-    expect(ans.author).not.to.equal(obj.author);
-    expect(ans.arrField).not.to.equal(obj.arrField);
-    expect(ans.config1.a).not.to.equal(obj.config1.a);
+        }).throw(/(read only)|(not extensible)/);
+        expect(() => {
 
-    // frozen
-    expect(() => {
+            ans.author.username = "shalgham";
 
-        ans.admin = false;
+        }).throw(/(read only)|(not extensible)/);
+        expect(() => {
 
-    }).throw();
+            ans.arrField.push(34);
 
-    expect(() => {
+        }).throw(/(read only)|(not extensible)/);
+        expect(() => {
 
-        ans.admin = true;
+            ans.arrField.splice(1, 1, "hom");
 
-    }).throw();
+        }).throw(/(read only)|(not extensible)/);
 
-    // no effect
-    obj.author.username = "new-name";
-    expect(ans.author.username).to.equal("joesmith");
-    obj.arrField.push("ggg");
-    expect(ans.arrField.length).to.equal(4);
-
-    /*try {
-        ans.config1.a.t = 2;
-    }
-    catch(error) {
-        console.log(error);
-    }*/
-
-    // deep frozen
-    expect(() => {
-
-        ans.config1.a.t = 2;
-
-    }).throw(/(read only)|(not extensible)/);
-    expect(() => {
-
-        ans.author.username = "shalgham";
-
-    }).throw(/(read only)|(not extensible)/);
-    expect(() => {
-
-        ans.arrField.push(34);
-
-    }).throw(/(read only)|(not extensible)/);
-    expect(() => {
-
-        ans.arrField.splice(1, 1, "hom");
-
-    }).throw(/(read only)|(not extensible)/);
+    });
 
 });
--- packages/@pinglue/utils/src/object-utils/defualt.spec.ts
+++ packages/@pinglue/utils/src/object-utils/defualt.spec.ts
@@ -3,212 +3,216 @@ import { _default } from "./index.js";
 
 let x1, x1Copy, x2, x2Copy;
 
-beforeEach(() => {
+describe("Test Merge functions", () => {
 
-    x1 = {
-        admin: true,
-        arrField: ["hey"],
-        author: {
-            name: { first: "Joe" }
-        }
-    };
+    beforeEach(() => {
 
-    x1Copy = {
-        admin: true,
-        arrField: ["hey"],
-        author: {
-            name: { first: "Joe" }
-        }
-    };
+        x1 = {
+            admin: true,
+            arrField: ["hey"],
+            author: {
+                name: { first: "Joe" }
+            }
+        };
 
-    x2 = {
-        admin: false,
-        arrField: ["hey1"],
-        author: {
-            name: { last: "Smith" },
-            username: "joesmith"
-        }
-    };
+        x1Copy = {
+            admin: true,
+            arrField: ["hey"],
+            author: {
+                name: { first: "Joe" }
+            }
+        };
 
-    x2Copy = {
-        admin: false,
-        arrField: ["hey1"],
-        author: {
-            name: { last: "Smith" },
-            username: "joesmith"
-        }
-    };
+        x2 = {
+            admin: false,
+            arrField: ["hey1"],
+            author: {
+                name: { last: "Smith" },
+                username: "joesmith"
+            }
+        };
 
-});
+        x2Copy = {
+            admin: false,
+            arrField: ["hey1"],
+            author: {
+                name: { last: "Smith" },
+                username: "joesmith"
+            }
+        };
 
-it("Defaulting two objects - simple", () => {
-
-    const ans = _default(x1, x2);
-
-    expect(ans).to.deep.equal(x1);
-    expect(x1).not.to.deep.equal(x1Copy);
-    expect(x2).to.deep.equal(x2Copy);
-
-    expect(ans).to.deep.equal({
-        admin: true,
-        arrField: ["hey"],
-        author: {
-            name: { last: "Smith", first: "Joe" },
-            username: "joesmith"
-        }
     });
 
-});
+    it("Defaulting two objects - simple", () => {
 
-it("Defaulting two objects into an empty object (deep cloning)", () => {
+        const ans = _default(x1, x2);
 
-    const ans = _default({}, x1, x2);
+        expect(ans).to.deep.equal(x1);
+        expect(x1).not.to.deep.equal(x1Copy);
+        expect(x2).to.deep.equal(x2Copy);
 
-    expect(ans).not.to.equal(x1);
-    expect(ans.author).not.to.equal(x1.author);
+        expect(ans).to.deep.equal({
+            admin: true,
+            arrField: ["hey"],
+            author: {
+                name: { last: "Smith", first: "Joe" },
+                username: "joesmith"
+            }
+        });
 
-    expect(x1).to.deep.equal(x1Copy);
-    expect(x2).to.deep.equal(x2Copy);
-
-    expect(ans).to.deep.equal({
-        admin: true,
-        arrField: ["hey"],
-        author: {
-            name: { last: "Smith", first: "Joe" },
-            username: "joesmith"
-        }
     });
 
-});
+    it("Defaulting two objects into an empty object (deep cloning)", () => {
 
-it("Merging two objects into a non-empty object with deep nesting)", () => {
+        const ans = _default({}, x1, x2);
 
-    const x0 = {
-        admin: true,
-        special: 1,
-        arrField: ["hey"],
-        author: {
-            name: { first: "Joe0", middle: "kalbali" },
-            username: "joesmith333"
-        }
-    };
+        expect(ans).not.to.equal(x1);
+        expect(ans.author).not.to.equal(x1.author);
 
-    const x0Copy = {
-        admin: true,
-        special: 1,
-        arrField: ["hey"],
-        author: {
-            name: { first: "Joe0", middle: "kalbali" },
-            username: "joesmith333"
-        }
-    };
+        expect(x1).to.deep.equal(x1Copy);
+        expect(x2).to.deep.equal(x2Copy);
 
-    const ans = _default(x0, x1, x2);
+        expect(ans).to.deep.equal({
+            admin: true,
+            arrField: ["hey"],
+            author: {
+                name: { last: "Smith", first: "Joe" },
+                username: "joesmith"
+            }
+        });
 
-    expect(ans).to.deep.equal(x0);
-    expect(ans).not.to.deep.equal(x1);
-    expect(ans).not.to.deep.equal(x2);
-
-    expect(x0).not.to.deep.equal(x0Copy);
-    expect(x1).to.deep.equal(x1Copy);
-    expect(x2).to.deep.equal(x2Copy);
-
-    expect(ans).to.deep.equal({
-        admin: true,
-        special: 1,
-        arrField: ["hey"],
-        author: {
-            name: { last: "Smith", first: "Joe0", middle: "kalbali" },
-            username: "joesmith333"
-        }
     });
 
-});
+    it("Merging two objects into a non-empty object with deep nesting)", () => {
 
-it("Deep nesting with some empty objs", () => {
+        const x0 = {
+            admin: true,
+            special: 1,
+            arrField: ["hey"],
+            author: {
+                name: { first: "Joe0", middle: "kalbali" },
+                username: "joesmith333"
+            }
+        };
 
-    const target = {
-        a: 1,
-        b: "b",
-        c: {
-            d: {
-                e: 1,
-                f: ["1", 2],
-                g: { h: [{ i: 1 }, "j"] }
+        const x0Copy = {
+            admin: true,
+            special: 1,
+            arrField: ["hey"],
+            author: {
+                name: { first: "Joe0", middle: "kalbali" },
+                username: "joesmith333"
+            }
+        };
+
+        const ans = _default(x0, x1, x2);
+
+        expect(ans).to.deep.equal(x0);
+        expect(ans).not.to.deep.equal(x1);
+        expect(ans).not.to.deep.equal(x2);
+
+        expect(x0).not.to.deep.equal(x0Copy);
+        expect(x1).to.deep.equal(x1Copy);
+        expect(x2).to.deep.equal(x2Copy);
+
+        expect(ans).to.deep.equal({
+            admin: true,
+            special: 1,
+            arrField: ["hey"],
+            author: {
+                name: { last: "Smith", first: "Joe0", middle: "kalbali" },
+                username: "joesmith333"
+            }
+        });
+
+    });
+
+    it("Deep nesting with some empty objs", () => {
+
+        const target = {
+            a: 1,
+            b: "b",
+            c: {
+                d: {
+                    e: 1,
+                    f: ["1", 2],
+                    g: { h: [{ i: 1 }, "j"] }
+                },
+                k: 1
             },
-            k: 1
-        },
-        l: {
-            m: {
-                n: {
-                    o: {
-                        p: 1
+            l: {
+                m: {
+                    n: {
+                        o: {
+                            p: 1
+                        }
                     }
                 }
             }
-        }
-    };
+        };
 
-    _default(
-        target,
-        { a: 2, c: { aa: 1 } },
-        null,
-        { c: { aa: 2 }, l: { m: { n: { oo: null } } } },
-        { c: { d: { f: [1] } } },
-        undefined,
-        null,
-        // @ts-expect-error
-        { l: { m: { n: { oo: 6 } } }, l: { m: { n: { oo2: 5 } } } },
-        undefined
-    );
+        _default(
+            target,
+            { a: 2, c: { aa: 1 } },
+            null,
+            { c: { aa: 2 }, l: { m: { n: { oo: null } } } },
+            { c: { d: { f: [1] } } },
+            undefined,
+            null,
+            // @ts-expect-error
+            { l: { m: { n: { oo: 6 } } }, l: { m: { n: { oo2: 5 } } } },
+            undefined
+        );
 
-    expect(target).not.to.equal({
-        a: 1,
-        b: "b",
-        c: {
-            d: {
-                e: 1,
-                f: ["1", 2],
-                g: { h: [{ i: 1 }, "j"] }
+        expect(target).to.deep.equal({
+            a: 1,
+            b: "b",
+            c: {
+                d: {
+                    e: 1,
+                    f: ["1", 2],
+                    g: { h: [{ i: 1 }, "j"] }
+                },
+                k: 1,
+                aa: 1
             },
-            k: 1,
-            aa: 1
-        },
-        l: {
-            m: {
-                n: {
-                    o: {
-                        p: 1
-                    },
-                    oo: null,
-                    oo2: 5
+            l: {
+                m: {
+                    n: {
+                        o: {
+                            p: 1
+                        },
+                        oo: null,
+                        oo2: 5
+                    }
                 }
             }
-        }
+        });
+
     });
 
-});
+    it("Error cases, wrong types", () => {
 
-it("Error cases, wrong types", () => {
+        expect(() => {
 
-    expect(() => {
+            _default(null, { b: 1 });
 
-        _default(null, { b: 1 });
+        }).to.throw("err-empty-target");
 
-    }).to.throw("err-empty-target");
+        expect(() => {
 
-    expect(() => {
+            // @ts-expect-error
+            _default(2, { b: 1 });
 
-        // @ts-expect-error
-        _default(2, { b: 1 });
+        }).to.throw("err-target-not-obj");
 
-    }).to.throw("err-target-not-obj");
+        expect(() => {
 
-    expect(() => {
+            // @ts-expect-error
+            _default({ a: 1 }, 2);
 
-        // @ts-expect-error
-        _default({ a: 1 }, 2);
+        }).to.throw("err-obj-not-obj");
 
-    }).to.throw("err-obj-not-obj");
+    });
 
 });
